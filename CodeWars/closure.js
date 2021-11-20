72e152a22bb5c08078f515ddbc35c0059bec5bd6
Yurecky16 <87438715+Yurecky16@users.noreply.github.com>
2021-11-20 23:42:16 +0700

    Решение задачи на замыкание без использования let

--- CodeWars/closure.js
+++ CodeWars/closure.js
@@ -3,13 +3,10 @@ function buildFun(n) {
 	var res = []
 
 	for (var i = 0; i < n; i++) {
-		let j = i;
-		res.push(function () {
-			return j;
-		})
-	}
+		(function () {
+			var j = i;
+			res.push(function () { return j; });
+		})();
+	};
 	return res
 }
-
-let test = buildFun(10);
-console.log(test)
